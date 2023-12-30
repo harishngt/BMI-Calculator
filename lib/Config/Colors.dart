c4e8c628d3a9afc84db9a17787189dca56b0f6ed
harishngt <97893600+harishngt@users.noreply.github.com>
2023-12-30 19:09:40 +0530

    Update Colors.dart

--- lib/Config/Colors.dart
+++ lib/Config/Colors.dart
@@ -7,7 +7,7 @@ const LDivColor = Color(0xffFFFFFF);
 const LFontColor = Color(0xff000000);
 
 const DBgColor = Color(0xff242424);
-const DPrimaryColor = Color(0xff373737);
+const DPrimaryColor = Color(0xff246AFE);
 const DLableColor = Color(0xff8C8C8C);
 const DDivColor = Color(0xff373737);
 const DFontColor = Color(0xffFFFFFF);
