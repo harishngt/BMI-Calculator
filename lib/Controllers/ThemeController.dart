5c445a89118a717fff979953a8c51e00b9f0d080
harishngt <97893600+harishngt@users.noreply.github.com>
2023-12-30 19:11:47 +0530

    Update ThemeController.dart

--- lib/Controllers/ThemeController.dart
+++ lib/Controllers/ThemeController.dart
@@ -5,10 +5,12 @@ class ThemeController extends GetxController {
   RxBool isDark = false.obs;
 
   void changeThemeDark() async {
+    isDark.value = !isDark.value;
     Get.changeThemeMode(ThemeMode.dark);
   }
 
   void changeThemeLight() async {
+    isDark.value = !isDark.value;
     Get.changeThemeMode(ThemeMode.light);
   }
 }
