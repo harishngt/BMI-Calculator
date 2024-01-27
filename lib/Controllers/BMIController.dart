45bce7b3db9e5cd6eebde54d3149e7c45d9e6a2d
harishngt <97893600+harishngt@users.noreply.github.com>
2024-01-27 18:29:14 +0530

    Update BMIController.dart

--- lib/Controllers/BMIController.dart
+++ lib/Controllers/BMIController.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 class BMIController extends GetxController {
   RxString Gender = "Male".obs;
+  RxInt weight = 12.obs;
+  RxInt age = 12.obs;
   void genderHandle(String gender) {
     Gender.value = gender;
   }
